Need to fix hessian, see barriers.py file. if x is all 0s then perturb x with a little noise.

OptimisticPricing.py
@@ -5,6 +5,7 @@ from BanditPricing import *
 from argminCVX import *
 from scipy.linalg import sqrtm
 
+
 def MOPOL(demands_prev, eta, delta, k, s_radius, prev_state, barrier, hessian):
     """ Modified Low-rank dynamic pricing with *Unknown* product features (latent U is assumed orthonormal).
 
@@ -24,7 +25,7 @@ def MOPOL(demands_prev, eta, delta, k, s_radius, prev_state, barrier, hessian):
 
     # read prev data
     x_prev_clean, Q, t, update_cnts, xi_prev, p_prev, g_aggr_prev = prev_state
-
+    print(x_prev_clean)
     d = x_prev_clean.shape[0]
     N = p_prev.shape[0]
     # Q update here:
@@ -41,6 +42,7 @@ def MOPOL(demands_prev, eta, delta, k, s_radius, prev_state, barrier, hessian):
         Uhat, singval, right_singvec = np.linalg.svd(Q, full_matrices=False)
         x_prev_clean = np.dot(Uhat.transpose(), p_prev)  # first low-dimensional action.
         next_state = (x_prev_clean, Q, t + 1, update_cnts, xi_prev, p_prev, g_aggr_prev)
+        print('randround')
         return ((p_rand, next_state))
 
     # Otherwise run our algorithm: we have xi_prev from before
@@ -49,7 +51,11 @@ def MOPOL(demands_prev, eta, delta, k, s_radius, prev_state, barrier, hessian):
 
     #g_aggr_prev = (list of g_hats up to t-1, g_bar_1:t-1 (sum of subgradients up to t-1)
     hat_list, g_bar_prev = g_aggr_prev
+    print(x_prev_clean)
+    print('blank')
+    print(hessian(x_prev_clean))
 
+    #TODO: hessian returning NaN when x is all zeros
     g_hat = d / delta * R_prev @ sqrtm(hessian(x_prev_clean)) @ xi_prev
     hat_list.append(g_hat)
 
@@ -65,7 +71,7 @@ def MOPOL(demands_prev, eta, delta, k, s_radius, prev_state, barrier, hessian):
     #setting up next iteration + getting prices from prev
     xi_next = randUnitVector(d) #sample UAR from sphere
 
-    x_tilde = x_next_clean + delta * sqrtm(hessian(x_prev_clean)) @ xi_next
+    x_tilde = x_next_clean + delta * sqrtm(hessian(x_next_clean)) @ xi_next
     g_aggr_next = (hat_list, g_bar_aggr_t)
 
     p_tilde = findPrice(x_tilde, Uhat)
@@ -84,7 +90,7 @@ def firstMOPOLstate(d, p_init):
     """
     N = p_init.shape[0]
     Q = np.zeros((N, d))
-    x0 = np.zeros(d)
+    x0 = np.ones(d)
     t0 = 0
     update_cnts = np.zeros(d)
     xi0 = randUnitVector(d)

barriers.py
@@ -2,7 +2,7 @@
 
 
 import autograd.numpy as np  # Thinly-wrapped numpy
-from autograd import grad
+from autograd import hessian
 
 # TODO: easiest way: hardcode r into this file in ball_barrier r = radius of S
 # see Lemma A.2 in Mueller
@@ -11,4 +11,10 @@ def ball_barrier_20(x):
     return -1 * np.log(20**2 - np.linalg.norm(x)**2)
 
 def hessian_ball_20(): #need to define function in the simulation file somehow
-    return grad(grad(ball_barrier_r)) #hessian of f
+    return hessian(ball_barrier_20) #hessian of f
+#TODO:
+def hessian(x, hess):
+    # to fix problem with NaN hessians
+    if x is all zeros
+    #perturb x
+    return hess(x)

example.py
@@ -97,7 +97,7 @@ for rep in range(nrep): #number of simulations
                     inferred_rank = d  # run with correctly specifed rank, can change this value to see the effects of wrongly-inferred rank.
                     p_t, opol_state = OPOL(init_demands, eta, delta, s_radius, firstOPOLstate(inferred_rank, p_init))
             elif method == 1:  # MOPOL pricing
-                barrier, hessian = br.ball_barrier_20, br.hessian_ball_20
+                barrier, hessian = br.ball_barrier_20, br.hessian_ball_20()
 
                 eta = C * np.power(t_touse, -3 / 4) * np.power(d, -1 / 2) / (
                     R_bound * s_radius * np.sqrt((1 + s_radius) * (3*s_radius + 2)))
@@ -106,11 +106,11 @@ for rep in range(nrep): #number of simulations
                     np.sqrt((3*s_radius + 2) * (1 + s_radius)/((2*s_radius + 1) ** 2)))
                 k = C * (2* s_radius+ 1)/ ((3* s_radius + 2) * np.sqrt(s_radius * R_bound * (1+ s_radius)))
                 if t > 0:
-                    p_t, opol_state = MOPOL(opol_demand_prev, eta, delta, k,
-                                            s_radius, opol_state, barrier, hessian)
+                    p_t, mopol_state = MOPOL(mopol_demand_prev, eta, delta, k,
+                                            s_radius, mopol_state, barrier, hessian)
                 else:
                     inferred_rank = d  # run with correctly specifed rank, can change this value to see the effects of wrongly-inferred rank.
-                    p_t, opol_state = MOPOL(opol_demand_prev, eta, delta, k,
+                    p_t, mopol_state = MOPOL(init_demands, eta, delta, k,
                                             s_radius, firstMOPOLstate(inferred_rank, p_init), barrier, hessian)
 
             # OTHER UNNEEDED METHODS
@@ -147,14 +147,12 @@ for rep in range(nrep): #number of simulations
             q_t = generateDemands(p_t, U, z, V, noise_std)  # feed in vector of observed demands here.
             revenues[method, t, rep] = negRevenue(p_t, q_t)
             x_t = np.dot(U.transpose(), p_t)
-            if method == 1:  # GDG pricing
-                gdg_revenue_prev = negRevenue(p_t, q_t)
-            if method == 3:  # OPOL pricing
+
+            if method == 0:  # OPOL pricing
                 opol_demand_prev = q_t
-            elif method == 4:
-                toobig_demand_prev = q_t
-            elif method == 5:
-                toosmall_demand_prev = q_t
+            if method == 1:  # MOPOL pricing
+                mopol_demand_prev = q_t
+
             if (t + 1) in summary_times:
                 regrets[method, regret_index, rep] = np.sum(revenues[method, :, rep]) - R_star * (
                             t + 1)  # *(t+1) only needed for stationary underlying model.